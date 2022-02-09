2a6fd52410c3a701ac9fef23f9c20dae7e7d95c5
Fernando <fernando.viana.nt@gmail.com>
2022-02-09 17:14:22 -0300

    filters

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import { BsFillGearFill, BsArrowDownShort, BsArrowUpShort } from 'react-icons/bs';
 import {
@@ -28,6 +28,10 @@ interface MealProps {
 export function User() {
   const [mealsData, setMealsData] = useState([]);
   const [recentMeals, setRecentMeals] = useState(true);
+  const [vegetablesFilter, setVegetablesFilter] = useState('');
+  const [proteinsFilter, setProteinsFilter] = useState('');
+  const [carbohydratesFilter, setCarbohydratesFilter] = useState('');
+  const [mealFilter, setMealFilter] = useState('');
 
   const { id } = useParams();
 
@@ -44,14 +48,19 @@ export function User() {
       .then(async (response: any) => {
         setMealsData(response.data);
       });
-    // .catch(() => {
-    //   window.location.href = `${urlConfig.frontendURL}/`;
-    // });
   }, []);
+
+  const filtered = useMemo(() => mealsData.filter((meal: MealProps) => (
+    meal.carbohydratefood.toLowerCase().includes(carbohydratesFilter.toLowerCase())
+    && meal.proteinfood.toLowerCase().includes(proteinsFilter.toLowerCase())
+    && meal.vegetablefood.toLowerCase().includes(vegetablesFilter.toLowerCase())
+    && meal.meal.toLowerCase().includes(mealFilter.toLowerCase())
+  )), [carbohydratesFilter, proteinsFilter, vegetablesFilter, mealFilter]);
 
   return (
     <Container>
       <Base meals createmeal={false}>
+
         <SectionContent>
           <h1>Todas as Refeições</h1>
           <Filters>
@@ -60,11 +69,14 @@ export function User() {
                 <tbody>
                   <tr>
                     <td>
-                      <Select>
+                      <Select
+                        value={mealFilter}
+                        onChange={(event) => setMealFilter(event.target.value)}
+                      >
                         <option value="">Refeição</option>
-                        <option value="">Café da manhã</option>
-                        <option value="">Almoço</option>
-                        <option value="">Janta</option>
+                        <option value="café da manha">Café da manhã</option>
+                        <option value="almoço">Almoço</option>
+                        <option value="almoço">Janta</option>
                       </Select>
                     </td>
                     <td>
@@ -76,35 +88,44 @@ export function User() {
                       </button>
                     </td>
                     <td>
-                      <Select>
+                      <Select
+                        value={proteinsFilter}
+                        onChange={(event) => setProteinsFilter(event.target.value)}
+                      >
                         <option value="">Proteína</option>
-                        <option value="">Peixe</option>
-                        <option value="">Boi</option>
-                        <option value="">Porco</option>
-                        <option value="">Frango</option>
-                        <option value="">Ovo</option>
+                        <option value="peixe">Peixe</option>
+                        <option value="boi">Boi</option>
+                        <option value="porco">Porco</option>
+                        <option value="frango">Frango</option>
+                        <option value="ovo">Ovo</option>
                       </Select>
                     </td>
                     <td><button type="button">Qntd.</button></td>
                     <td>
-                      <Select>
+                      <Select
+                        value={carbohydratesFilter}
+                        onChange={(event) => setCarbohydratesFilter(event.target.value)}
+                      >
                         <option value="">Carboidrato</option>
-                        <option value="">Arroz</option>
-                        <option value="">Batata</option>
-                        <option value="">Inhame</option>
-                        <option value="">Macarrão</option>
-                        <option value="">Pão</option>
+                        <option value="arroz">Arroz</option>
+                        <option value="batata">Batata</option>
+                        <option value="inhame">Inhame</option>
+                        <option value="macarrão">Macarrão</option>
+                        <option value="pão">Pão</option>
                       </Select>
                     </td>
                     <td><button type="button">Qntd.</button></td>
                     <td>
-                      <Select>
+                      <Select
+                        value={vegetablesFilter}
+                        onChange={(event) => setVegetablesFilter(event.target.value)}
+                      >
                         <option value="">Vegetais</option>
-                        <option value="">Abobrinha</option>
-                        <option value="">Berinjela</option>
-                        <option value="">Abóbora</option>
-                        <option value="">Almeirão</option>
-                        <option value="">Tomate</option>
+                        <option value="abobrinha">Abobrinha</option>
+                        <option value="berinjela">Berinjela</option>
+                        <option value="abobora">Abóbora</option>
+                        <option value="almeirão">Almeirão</option>
+                        <option value="tomate">Tomate</option>
                       </Select>
                     </td>
                     <td><button type="button">Qntd.</button></td>
@@ -119,7 +140,7 @@ export function User() {
           </Filters>
           <MealsContainer>
             {
-              mealsData.map((meal: MealProps) => (
+              filtered.map((meal: MealProps) => (
                 <MealCard
                   vegetablesamount={meal.vegetablesamount}
                   carbohydratesamount={meal.carbohydratesamount}
